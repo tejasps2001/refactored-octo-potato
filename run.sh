4b5a1585f16860527936bf2d7952e34778e7e9a3
Tejas P S <tejasps2001@gmail.com>
2026-02-26 11:51:51 +0530

    Fix the bash by checking if the venv exists first

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Create a venv and install from the requirements.txt
-python3 -m venv ./ai_tutor
-source ai_tutor/bin/activate
-python3 -m pip install -r requirements.txt
+VENV_PATH="./ai_tutor"
 
-cd /Gesture_Recognizer
+# Check if the venv directory already exists
+if [ ! -d "$VENV_PATH" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv "$VENV_PATH"
+    source "$VENV_PATH/bin/activate"
+    python3 -m pip install -r requirements.txt
+else
+    echo "Virtual environment already exists. Skipping the installation."
+    source "$VENV_PATH/bin/activate"
+fi
 
-python3 run_camera.py
+cd ./Gesture_Recognizer
+python3 ./run_camera.py
